1c9232193e62032a6098af388d6ce539740eb3a4
huy2009-ux <quanghuy2009s@gmail.com>
2026-06-07 13:37:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ async function sendOTP(email, otp) {
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET || 'secret',
